5cafdae48f06fab1d378ff5b41e4751b9a1cadbc
tonguiino <santiagotq2@gmail.com>
2026-04-10 14:22:15 -0500

    feat: integracion y definicion de endpoints para TicketsControllers

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TicketController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('tickets', TicketController::class);
